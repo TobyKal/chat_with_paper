d386f4b667791fd24615ce9467a2c0ad6a597282
Tobiasz Kaluzny <k4luznytobiasz@gmail.com>
2024-04-30 05:01:08 +0100

    0.5.3v
    thinker, responder and streaming. Trying to figure out how to make the agents complaint.

--- llm_interfaces.py
+++ llm_interfaces.py
@@ -62,20 +62,39 @@ class From_antropic:
 class Form_groq:
     '''
     This class lets you choose what models
-    avelable at groq you want to recive response
+    (avelable at groq) you want to recive response
     '''
     def __init__(self) -> None:
         load_dotenv()
         self.key = os.getenv('GROQ')
+        self.client = Groq(api_key=self.key)
+
+    def send_streaming_request(self, system_prompt: str, context: list[dict], model: str, max_tokens: int):       
+        template = {"role": "system", "content": f"{system_prompt}"}
+        context.insert(0, template)
+        
+        text = ''
+        stream = self.client.chat.completions.create(
+            messages=context,
+            model=model,
+            max_tokens=max_tokens,
+            stream=True,
+        )
+        
+        for chunk in stream:
+            content = chunk.choices[0].delta.content
+            if content != None:
+                text += content
+                print(content, end="")
+        return text
 
 
     def send_request(self, system_prompt: str, context: list[dict], model: str, max_tokens: int) -> str:
         template = {"role": "system", "content": f"{system_prompt}"}
         context.insert(0, template)
         
-        client = Groq(api_key=self.key)
 
-        response = client.chat.completions.create(
+        response = self.client.chat.completions.create(
             model=model,
             messages=context, 
             max_tokens=max_tokens
@@ -83,20 +102,35 @@ class Form_groq:
         return response.choices[0].message.content
 
 
-    def llama3_8b(self, system_prompt: str, context: list[dict]) -> str:
-        return self.send_request(system_prompt, context, "llama3-8b-8192", 8192)
+    def llama3_8b(self, system_prompt: str, context: list[dict], stream: bool = False) -> str:
+        if stream:
+            return self.send_streaming_request(system_prompt, context, "llama3-8b-8192", 8192)
+        else:
+            return self.send_request(system_prompt, context, "llama3-8b-8192", 8192)
 
-    def llama3_70b(self, system_prompt: str, context: list[dict]) -> str:
-        return self.send_request(system_prompt, context, "llama3-70b-8192", 8192)
+    def llama3_70b(self, system_prompt: str, context: list[dict], stream: bool = False) -> str:
+        if stream:
+            return self.send_streaming_request(system_prompt, context, "llama3-70b-8192", 8192)
+        else:
+            return self.send_request(system_prompt, context, "llama3-70b-8192", 8192)
     
-    def llama2_70b(self, system_prompt: str, context: list[dict]) -> str:
-        return self.send_request(system_prompt, context, "llama2-70b-4096", 4096)
+    def llama2_70b(self, system_prompt: str, context: list[dict], stream: bool = False) -> str:
+        if stream:
+            return self.send_streaming_request(system_prompt, context, "llama2-70b-4096", 4096)
+        else:    
+            return self.send_request(system_prompt, context, "llama2-70b-4096", 4096)
 
-    def gemma_7B(self, system_prompt: str, context: list[dict]) -> str:
-        return self.send_request(system_prompt, context, "gemma-7b-it", 8192)
+    def gemma_7B(self, system_prompt: str, context: list[dict], stream: bool = False) -> str:
+        if stream:
+            return self.send_streaming_request(system_prompt, context, "gemma-7b-it", 8192)
+        else:
+            return self.send_request(system_prompt, context, "gemma-7b-it", 8192)
 
-    def mixtral_8x7B(self, system_prompt: str, context: list[dict]) -> str:
-        return self.send_request(system_prompt, context, "mixtral-8x7b-32768", 32768)
+    def mixtral_8x7B(self, system_prompt: str, context: list[dict], stream: bool = False) -> str:
+        if stream:
+            return self.send_streaming_request(system_prompt, context, "mixtral-8x7b-32768", 32768)
+        else:    
+            return self.send_request(system_prompt, context, "mixtral-8x7b-32768", 32768)
 
 
 class Agents:
@@ -138,16 +172,45 @@ class Agents:
         return response 
     
     def question_asker(self, text: str) -> list[str]:
-        '''This agent is focused on creating QUESTIONS THAT ANSWER CAN BE FOUND
+        '''
+        This agent is focused on creating QUESTIONS THAT ANSWER CAN BE FOUND
         in a supplied text and returns a list of stings with infromations
-        if all of the tries fail, it will return an empty list'''
+        if all of the tries fail, it will return an empty list
+        '''
 
         system_prompt = """You are an advanced algorithm that specializes in
         creating questions, form a recived information, answers to with you can
         find in that infrmation. you are only allowed to respond in the form of a Python list of strings, 
         where each string should contain a question do not repeate questions.
         YOU ARE NOT ALLWED TO RESPOND IN ANY OTHER WAY EXCEPT PYTHON LIST"""
+
         response = self.send_request_return_type(system_prompt, text, "Question_asker", list)
         if response == None: return []
         return response 
     
+    def thinker(self, context: Context) -> list[str]:
+        '''
+        This agent is focused on THINKING this agent will will think about your prompt
+        it will try to figure out how to respond to your quesion.
+        '''
+        system_prompt = """
+        You are a large language model. You will be provided a user message.
+        I want you to analyze that message and do 'thinking' about it, think about what the user meant,
+        and how could you respond to it, be dense, short and practical with your thoughts. YOU ARE A THINKER, DO NOT ANSWER THE QUESTION OR PERFORM A TASK.
+        """
+        return Form_groq().llama3_70b(system_prompt, context)
+       
+    def responder(self, context: Context) -> str: 
+        '''
+        This agent is a final piece in the puzzle. He is supposed to summarize thoughts,
+        analyze documents or just simply respond to a question.
+        '''
+        system_prompt = """you are a helpful assistant. you will be provided users quesion
+        or request as well as your internal reflection reqarding users request. your task is
+        to help the user using all of your abilities DO NOT THINK, ANSWER 
+        """
+        return Form_groq().llama3_70b(system_prompt, context)
+
+
+
+
--- run.py
+++ run.py
@@ -1,117 +1,87 @@
-from llm_interfaces import context, form_groq, from_antropic
-from rich import print
-from rich.console import Console
-from rich.console import Console
-from rich.markdown import Markdown
-from rich.theme import Theme
+from llm_interfaces import Context, Form_groq, From_antropic, Agents
+import os
 
+def clear_terminal():
+    # For Windows
+    if os.name == 'nt':
+        os.system('cls')
 
-def clear_context() -> context:
-    print(f"\033[1m You've cleared the context: \033[0m")
-    return context()
+def clear_context() -> Context:
+    clear_terminal()
+    print("You've cleared the context:")
+    return Context()
 
 def new_system_prompt() -> str:
-    c.print(f"Type new system prompt:\n", style="bold magenta")
-    new = input('')
-    if len(new) <5:
-        c.print(f"\n Your new prompt was too short at least 5 characters!", style="bold magenta")
+    new = input("Type new system prompt:\n")
+    if len(new) < 5:
+        print("Your new prompt was too short, at least 5 characters!\n")
         return new_system_prompt()
-    c.print(f"Your new system prompt is {new}\n", style="bold magenta")
+    print(f"Your new system prompt is {new}\n\n Heloo, how can i help you??\n")
     return new
 
-
-def chat(system_prompt: str, user_prompt: str, chat_log: context, model) -> str:
+def chat(user_prompt: str, chat_log: Context) -> str:
     chat_log.add_user_message(user_prompt)
-    response = model(system_prompt, chat_log.to_list())
+    response = Agents().thinker(chat_log.to_list())
+    print(response)
+    chat_log.delete_last_message()
+    new = f"[user]{user_prompt}[/user][thinking]{response}[/thinking][response]"
+    chat_log.add_user_message(new)
+    response = Agents().responder(chat_log.to_list())
     chat_log.add_assistant_message(response)
     return response
 
-
 def choice_menu_logic():
     while True:
-            c.print(model_menu)
-            user_input = input("\n")
-            if user_input == "haiku":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return from_antropic().haiku
-                
-            elif user_input == "sonnet":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return from_antropic().sonnet
-
-            elif user_input == "opus":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return from_antropic().opus
-                
-            elif user_input == "llama3-8b":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return form_groq().llama3_8b
-
-            elif user_input == "llama3-70b":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return form_groq().llama3_70b
-            
-            elif user_input == "llama2-70b":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return form_groq().llama2_70b
-                
-            elif user_input == "gemma-7B":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return form_groq().gemma_7B
-                
-            elif user_input == "mixtral-8x7B":
-                c.print(f"\nYou are using {user_input} have fun!!", style="bold magenta")
-                return form_groq().mixtral_8x7B
-                
-            else:
-                c.print(f"Invalid model name. Please try again.", style="bold red")
-
+        user_input = input("\nEnter the model name: ")
+        if user_input == "haiku":
+            return From_antropic().haiku
+        elif user_input == "sonnet":
+            return From_antropic().sonnet
+        elif user_input == "opus":
+            return From_antropic().opus
+        elif user_input == "llama3-8b":
+            return Form_groq().llama3_8b
+        elif user_input == "llama3-70b":
+            return Form_groq().llama3_70b
+        elif user_input == "llama2-70b":
+            return Form_groq().llama2_70b
+        elif user_input == "gemma-7B":
+            return Form_groq().gemma_7B
+        elif user_input == "mixtral-8x7B":
+            return Form_groq().mixtral_8x7B
+        else:
+            print("Invalid model name. Please try again.")
 
 
 main_menu = """
-[menu_title]Welcome! What do you need to know boss?[/menu_title]        
-[option]\\q[/option] - Quit
-[option]\\s[/option] - Change system prompt default "Provide brief and precise answers. Elaborate only if requested."
-[option]\\m[/option] - Choose chat model default lamma3-70B
-[option]\\c[/option] - Clear llms context
-[option]\\mc[/option] - Choose model AND clear context
+Welcome! What do you need to know boss?[/menu_title]        
+\\q - Quit
+\\s- Change system prompt default "Provide brief and precise answers. Elaborate only if requested."
+\\m- Choose chat model default lamma3-70B
+\\c- Clear llms context
+\\mc - Choose model AND clear context
 Enter your command:
 """
 
 model_menu = """
-[menu_title]Choose a chat model from the list below:[/menu_title]
-[paid_model]haiku (paid!)[/paid_model]
-[paid_model]sonnet (paid!)[/paid_model]
-[paid_model]opus (paid!)[/paid_model]
-[model_name]llama3-8b[/model_name]
-[model_name]llama3-70b[/model_name]
-[model_name]llama2-70b[/model_name]
-[model_name]gemma-7B[/model_name]
-[model_name]mixtral-8x7B[/model_name]
-[input_prompt]Enter the model name:[/input_prompt] """
+Choose a chat model from the list below:[/menu_title]
+haiku (paid!)
+sonnet (paid!)
+opus (paid!)
+llama3-8b
+llama3-70b
+llama2-70b
+gemma-7B
+mixtral-8x7B
 
+Enter the models name """
 
+chat_log = Context()
+system_prompt = """You are a proactive and helpful assistant. provide answers to questions or to carry out the tasks efficiently.
+"""
+model = Form_groq().llama3_70b
 
-custom_theme = Theme({
-    "menu_title": "bold magenta",
-    "model_name": "bold cyan",
-    "paid_model": "bold yellow",
-    "input_prompt": "bold magenta",
-    "default": "italic",
-    "menu_title": "bold magenta",
-    "option": "bold cyan",
-    "command": "green",
-    "default": "italic"
-})
-c = Console(theme=custom_theme)
-
-
-
-system_prompt = "You are a helpfull assistan with a BRO personality, style you response in mark up but not indide code blocks"
-chat_log = context()
-model = form_groq().llama3_70b
-
-c.print(main_menu)
+print(main_menu)
 user_prompt = input()
 
 while True:
@@ -121,25 +91,25 @@ while True:
     elif user_prompt == "\\mc":
         chat_log = clear_context()
         model = choice_menu_logic()
-        c.print("\nWelcome! what do you need to know boss?\n", style= "#408000")
+        print("\nWelcome! What do you need to know boss?\n")
         user_prompt = input("")
 
     elif user_prompt == "\\m":
         model = choice_menu_logic()
-        c.print("\nWelcome! what do you need to know boss?\n", style= "#408000")
+        print("\nWelcome! What do you need to know boss?\n")
         user_prompt = input("")
     
     elif user_prompt == "\\s":
         system_prompt = new_system_prompt()
+        user_prompt = input("")
 
     elif user_prompt == "\\c":
         chat_log = clear_context()
-        c.print("Type your first message:\n", style= "bold magenta")
+        print("Type your first message:\n")
         user_prompt = input("")
 
     elif user_prompt == "" or None:
         user_prompt = input("")
-
     else:
-        c.print(Markdown(chat(system_prompt, user_prompt, chat_log, model)), style= "#408000")
-        user_prompt = input("\n")
+        print("\n", chat(user_prompt, chat_log))
+        user_prompt = input("\n\n")
